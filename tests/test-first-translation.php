01193ee6d7383617e8279379720fd466a482135b
Alex Kirk <akirk@users.noreply.github.com>
2024-12-03 19:30:42 +0100

    lint

--- tests/test-first-translation.php
+++ tests/test-first-translation.php
@@ -11,17 +11,24 @@ namespace Wporg\Tests;
  * Test the First_Translation class.
  */
 class First_Translation_Test extends Base_Test {
-
+	/**
+	 * Test the first translation notification.
+	 */
 	public function test_first_translation_notification() {
-		$user = $this->factory->user->create();
-		$approver = $this->factory->user->create();
+		$user         = $this->factory->user->create();
+		$approver     = $this->factory->user->create();
 		$translation1 = $this->factory->translation->create(
 			array(
 				'status'  => 'waiting',
 				'user_id' => $user,
 			)
 		);
-		$translation1->update( array( 'user_id_last_modified' => $approver, 'status' => 'current' ) );
+		$translation1->update(
+			array(
+				'user_id_last_modified' => $approver,
+				'status'                => 'current',
+			)
+		);
 		$translation1 = \GP::$translation->get( $translation1->id );
 
 		$first_translation = new \WordPressdotorg\GlotPress\Engagement\First_Translation();
@@ -45,9 +52,11 @@ class First_Translation_Test extends Base_Test {
 		$first_translation( $translation1 );
 
 		$this->assertEquals( 1, $mock->get_call_count() );
-
 	}
 
+	/**
+	 * Test the first translation hook notification.
+	 */
 	public function test_first_translation_hook_notification() {
 		$first_translation = new \WordPressdotorg\GlotPress\Engagement\First_Translation();
 		add_action( 'gp_translation_saved', array( $first_translation, '__invoke' ) );
@@ -58,8 +67,8 @@ class First_Translation_Test extends Base_Test {
 		$mock = new \MockAction();
 		add_action( 'wporg_translate_notification_first_translation', array( $mock, 'action' ), 10, 2 );
 
-		$user = $this->factory->user->create();
-		$approver = $this->factory->user->create();
+		$user         = $this->factory->user->create();
+		$approver     = $this->factory->user->create();
 		$translation1 = $this->factory->translation->create(
 			array(
 				'status'  => 'waiting',
@@ -67,7 +76,12 @@ class First_Translation_Test extends Base_Test {
 			)
 		);
 		$this->assertEquals( 0, $mock->get_call_count() );
-		$translation1->save( array( 'user_id_last_modified' => $approver, 'status' => 'current' ) );
+		$translation1->save(
+			array(
+				'user_id_last_modified' => $approver,
+				'status'                => 'current',
+			)
+		);
 		$translation1 = \GP::$translation->get( $translation1->id );
 
 		$this->assertEquals( 1, $mock->get_call_count() );
@@ -82,6 +96,5 @@ class First_Translation_Test extends Base_Test {
 		$first_translation( $translation1 );
 
 		$this->assertEquals( 1, $mock->get_call_count() );
-
 	}
 }
